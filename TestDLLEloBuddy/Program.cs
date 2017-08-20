6a77ef42c7d4ca2a4ef0c298cd7469aea3d66346
Julian Brns <kappatwitchalert@gmail.com>
2017-08-20 16:41:56 +0200

    u

--- TestDLLEloBuddy/Program.cs
+++ TestDLLEloBuddy/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+using EloBuddy;
+using EloBuddy.SDK.Events;
 
 namespace TestDLLEloBuddy
 {
     class Program
     {
+        private static readonly string dllPath = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EloBuddy\Addons\Libraries\ChampionScriptDLLTest.dll";
         static void Main(string[] args)
         {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (File.Exists(dllPath))
+            {
+                File.Delete(dllPath);
+            }
+            var bydll = Assembly.LoadFrom(dllPath);
         }
     }
 }
