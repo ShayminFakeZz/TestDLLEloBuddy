406d4a24f7a7a0dfede62f748887c41371a38a97
Julian Brns <kappatwitchalert@gmail.com>
2017-08-20 16:14:53 +0200

    Projektdateien hinzufügen.

---
+++ TestDLLEloBuddy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDLLEloBuddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
